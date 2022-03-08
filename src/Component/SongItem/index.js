279f11e15c6dc937e785d080418ebaba621e45dc
pavan-vsp <vempulurupavankumar@gmail.com>
2022-03-08 10:17:00 +0000

    add data

---
+++ src/Component/SongItem/index.js
@@ -0,0 +1,31 @@
+import {AiTwotoneDelete} from 'react-icons/ai'
+import './index.css'
+
+const SongItem = props => {
+  const {songDetails, del} = props
+  const {id, imageUrl, name, genre, duration} = songDetails
+
+  const delFun = () => {
+    del(id)
+  }
+
+  return (
+    <li className="list-item-container">
+      <div className="details">
+        <img src={imageUrl} alt="track" className="song-image" />
+        <div className="song-details">
+          <p className="">{name}</p>
+          <p className="genre">{genre}</p>
+        </div>
+      </div>
+      <div className="duration-dltIcon">
+        <p>{duration}</p>
+        <button type="button" className="delete-btn" testid="delete">
+          <AiTwotoneDelete size={20} color="white" onClick={delFun} />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default SongItem
